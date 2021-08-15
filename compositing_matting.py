d6f6315cb611bd99f308d7338a4625e13c0265b9
dattrinh01 <tandattrinh2001@gmail.com>
2021-08-15 16:49:04 +0700

    Update-image-compositing

--- compositing_matting.py
+++ compositing_matting.py
@@ -9,13 +9,11 @@ aim = cv2.resize(aim, (199, 199))
 
 
 def compositing_and_matting(mark_img, aim_img, alpha):
-
     result = np.zeros((199, 199, 3), dtype="u1")
     for i in range(199):
         for j in range(199):
             temp = (1 - alpha) * mark_img[i, j] + alpha * aim_img[i, j]
             result[i, j] = temp
-
     return result
 
 
