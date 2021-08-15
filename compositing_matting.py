97f0b47d2737c69588ffe132a46f65fa0994d98b
dattrinh01 <tandattrinh2001@gmail.com>
2021-08-15 16:48:19 +0700

    Update-image-compositing

---
+++ compositing_matting.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+mark = cv2.imread("/home/dat/data/mask.png")
+aim = cv2.imread("/home/dat/data/aim.png")
+
+mark = cv2.resize(mark, (199, 199))
+aim = cv2.resize(aim, (199, 199))
+
+
+def compositing_and_matting(mark_img, aim_img, alpha):
+
+    result = np.zeros((199, 199, 3), dtype="u1")
+    for i in range(199):
+        for j in range(199):
+            temp = (1 - alpha) * mark_img[i, j] + alpha * aim_img[i, j]
+            result[i, j] = temp
+
+    return result
+
+
+result = compositing_and_matting(mark, aim, 0.5)
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- rotation.py
+++ rotation.py
@@ -6,28 +6,31 @@ import matplotlib.pyplot as plt
 def rotation_image(src_img, angle, shape_out_img):
 
     width, height = src_img.shape[0], src_img.shape[1]
-    R_mat = np.array([
-        [np.cos(angle), -np.sin(angle), 0],
-        [np.sin(angle), np.cos(angle), 0],
-        [0, 0, 1]
-    ])
+    # tranformation matrix
+    R_mat = np.array(
+        [
+            [np.cos(angle), -np.sin(angle), 0],
+            [np.sin(angle), np.cos(angle), 0],
+            [0, 0, 1],
+        ]
+    )
 
-    result_img = np.zeros(shape_out_img, dtype='u1')
+    result_img = np.zeros(shape_out_img, dtype="u1")
 
     for i in range(width):
         for j in range(height):
 
             _x = np.array([i, j, 1])
+            # homogenrous vector
             new_xy = np.dot(R_mat, _x)
 
             if 0 <= new_xy[0] <= width and 0 <= new_xy[1] <= height:
-                result_img[int(new_xy[0]), int(new_xy[1])
-                           ] = src_img[i, j]
+                result_img[int(new_xy[0]), int(new_xy[1])] = src_img[i, j]
 
     return result_img
 
 
-src_img = cv2.imread("lenna.png")
+src_img = cv2.imread("/home/dat/data/lenna.png")
 angle = 1
 shape_out_img = src_img.shape
 
