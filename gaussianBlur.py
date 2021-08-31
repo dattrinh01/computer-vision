021cde211282f3e54164a34e7cfcf35ed913abcb
dattrinh01 <tandattrinh2001@gmail.com>
2021-08-31 14:33:15 +0700

    Update-gaussian-blur

--- gaussianBlur.py
+++ gaussianBlur.py
@@ -4,42 +4,57 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 
 src_img = cv2.imread("lenna.png")
-src_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
 
-def gaussian_blur_1(kernel_size, sigma):
-    x = np.linspace(-sigma, sigma, kernel_size + 1)
-    x = np.diff(st.norm.cdf(x))
-    kernel = np.outer(x,x)
-    return kernel/kernel.sum()
+def gaussian(filter_size, sigma):
+    gaussian_filter = np.zeros((filter_size, filter_size), np.float32)
+    m = filter_size // 2
+    for x in range(-m, m+1):
+        for y in range(-m, m+1):
+            part_1 = 2 * np.pi * sigma**2
+            part_2 = np.exp(-(x**2 + y**2)/2*sigma**2)
+            gaussian_filter[x+m, y+m] = part_2 / part_1
+    return gaussian_filter
 
-def gaussian_blur_2(kernel_size, sigma):
-    kernel = np.ones((kernel_size,kernel_size))
-    kernel_size = kernel_size // 2
-    for x in range(-kernel_size, kernel_size + 1):
-        for y in range(-kernel_size, kernel_size + 1):
-            part_1 = np.exp(-(x**2+y**2)/(2*sigma**2))
-            part_2 = 2 * np.pi * sigma**2
-            kernel[x+kernel_size, y+kernel_size] = part_1 / part_2
-    return kernel
+def convolution(src_img, kernel):
 
-def conv2D(src_img, gau_mat, kernel_size):
-    [row, col] = src_img.shape
-    result = np.zeros(src_img.shape, dtype = "u1")
-    for i in range(kernel_size, row):
-        for j in range(kernel_size, col):
-            temp = src_img[i, j] * gau_mat
-            result[i,j] = temp.sum()
-    return result
+    kernel_h = kernel.shape[0]
+    kernel_w = kernel.shape[1]
 
+    if (len(src_img.shape) == 3):
+        pad_width = ((kernel_h//2, kernel_h//2) , (kernel_w//2, kernel_w//2) , (0,0))
+        pad_image = np.pad(src_img, pad_width = pad_width, mode = "constant", constant_values = 0).astype(np.float32)
+    elif (len(src_img.shape) == 2):
+        pad_width = ((kernel_h//2, kernel_h//2), (kernel_w//2, kernel_w//2))
+        pad_image = np.pad(src_img, pad_width = pad_width, mode = "constant", constant_values = 0).astype(np.float32)
+    
+    h = kernel_h // 2
+    w = kernel_w // 2
+    image_conv = np.zeros(pad_image.shape)
 
+    for i in range(h, pad_image.shape[0] - h):
+        for j in range(w, pad_image.shape[1] - w):
+            x = pad_image[i - h:i - h + kernel_h, j - w:j - w + kernel_w]
+            x = x.flatten() * kernel.flatten()
+            image_conv[i,j] = x.sum()
+    
+    h_end = -h
+    w_end = -w
 
-kernel1 = gaussian_blur_1(5,2)
-kernel2 = gaussian_blur_2(5,1.1)
-result_img_1 = cv2.filter2D(src_img,-1,kernel1)
-result_img_2 = cv2.filter2D(src_img,-1,kernel2)
+    if (h == 0):
+        return image_conv[h:, w:w_end]
+    if (w == 0):
+        return image_conv[h:h_end, w:]
+    
+    return image_conv[h:h_end, w:w_end]
 
+def process(src_image):
+    gaussian_filter = gaussian(5,1)
+    result_image = np.zeros(src_img.shape, dtype = "u1")
+    for c in range(3):
+        result_image[:, :, c] = convolution(src_img[:, :, c], gaussian_filter)
+    return result_image
+        
 
-cv2.imshow("img", result_img_1)
-cv2.imshow("img1",result_img_2)
+cv2.imshow("img", process(src_img))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
